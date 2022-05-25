a5347aaebe643cd0dc46f5ba92d33708e1139be4
FaberJoo <79781818+FaberJoo@users.noreply.github.com>
2022-05-25 16:03:20 +0900

    array and tuple types

--- basic/app.ts
+++ basic/app.ts
@@ -2,9 +2,33 @@
 //   name: string;
 //   age: number;
 // } 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Maxmilain',
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 }
 
+// person.role.push('admin');
+// person.role[1] = 10;
+
+person.role = [0, 'admin'];
+person.role.push('author');
+
+console.log(person.role);
+
+
+let favoriteActivities: any[];
+favoriteActivities = ['Sports', 1];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map());
+}
