be8525127132c962e9bb067dd197875b075463ae
FaberJoo <79781818+FaberJoo@users.noreply.github.com>
2022-05-26 20:25:15 +0900

    function type & callback

--- basic/app.ts
+++ basic/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log("Resulut " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 console.log(printResult(add(5, 12)));
 
 // const a: void = undefined;
@@ -19,3 +24,20 @@ combineValues = add;
 // combineValues = 5;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result: number) => {
+  console.log(result);
+});
+
+//
+function sendRequest(data: string, cb: (response: any) => void) {
+  // ... sending a request with "data"
+  return cb({ data: "Hi there!" });
+}
+
+const a = sendRequest("Send this!", (response) => {
+  console.log(response);
+  return true;
+});
+
+console.log(a);
